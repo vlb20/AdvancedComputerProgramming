e62b09c03bfe00af77a4b4881852b9cdede4ed1f
Vincenzo Luigi Bruno <73852936+vlb20@users.noreply.github.com>
2023-09-13 11:37:45 +0200

    Delete JAVA/EsercitazioneProxySkeleton(21)/EeProxySkeleton directory

--- JAVA/EsercitazioneProxySkeleton(21)/EeProxySkeleton/src/Server/Server.java
+++
@@ -1,9 +0,0 @@
-package Server;
-
-public class Server {
-    
-    public static void main(String[] args){
-        ServerImpl ser = new ServerImpl();
-        ser.runSkeleton(); //uso il serverImpl per ereditarietà per attivare runSkeleton
-    }
-}
--- JAVA/EsercitazioneProxySkeleton(21)/EeProxySkeleton/src/Servizio/IDispatcher.java
+++
@@ -1,9 +0,0 @@
-package Servizio;
-
-public interface IDispatcher {//interfaccia di esposizione del servizio!
-
-    //Interfaccia del servizio!!
-    public void sendCmd(Integer s);
-    public int getCmd();
-    
-}
